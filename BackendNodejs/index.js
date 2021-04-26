2b9627b8f8ee3097c65531241673453fefc2251c
Manh_Duc <duclemanh98@gmail.com>
2021-04-26 13:41:49 +0700

    update promises backend

--- BackendNodejs/index.js
+++ BackendNodejs/index.js
@@ -16,27 +16,94 @@ app.use(express.urlencoded( { extended: false}));
 
 var mysql = require('mysql');
 const pool = require('./pool');
+const { getConnection, query } = require('./pool');
 
-app.post('/userLogin', function(req, res) {
-    // res.header("Access-Control-Allow-Origin", "*");
-    // res.header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS'); 
-    // res.header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+
+/*
+ *  Function Prototype used
+ */
+function check_login(req) {
+    return new Promise(resolve => {
+        pool.query("CALL check_login(?, ?, @checking_val);SELECT @checking_val AS retval",[req.body.username, req.body.password], function(err, results){
+            if(err) throw err;
+            if(results[1][0].retval == 0) resolve(false);
+            else resolve(true);
+        });
+    })
+}
+
+//----------------------------------------------------------------------------------------//
+/*
+                        API for backend
+*/
+
+/*
+ *  @brief: API to check user login, request contain json included username and password
+ */
+app.post('/userLogin', async(req, res) => {
     console.log(req.body.username, req.body.password);
+    // if(check_login(req) == false) return res.json(false);
+    // else return res.json(true);
+    // pool.query("CALL check_login(?, ?, @checking_val);SELECT @checking_val AS retval",[req.body.username, req.body.password], function(err, result){
+    //     if(err) throw err;
+    //     if(result[1][0].retval == 0) {
+    //         console.log("No account");
+    //         return(res.json(false));
+    //         //return res.json(false);
+    //     }
+    //     else {
+    //         console.log("Correct Account");
+    //         return(res.json(true));
+    //         //return res.json()
+    //     }
+    // });
+    try {
+        var query_result = await check_login(req);
+        //console.log(query_result);
+        return res.json(query_result); 
+    }
+    catch(error) {
+        console.log(error);
+    }
+});
 
-    pool.query("CALL check_login(?, ?, @checking_val);SELECT @checking_val AS retval",[req.body.username, req.body.password], function(err, result){
-        if(err) throw err;
-        if(result[1][0].retval == 0) {
-            console.log("No account");
-            return(res.json(false));
-            //return res.json(false);
+/*
+ *  @brief: API to add user into database
+ *  request: json include: username, password, auth
+ */
+app.post('/userAdd', function(req, res){
+    pool.query('CALL add_user(?,?,?)',[req.body.username, req.body.password, req.body.auth], function(err, results){
+        if(err) {
+            return res.json(err);
+            throw err;
         }
-        else {
-            console.log("Correct Account");
-            return(res.json(true));
-            //return res.json()
-        }
+        console.log("Add successfully");
+        return (res.json(true));
     });
 });
+
+/*
+ *  @brief: API to delete user from database
+ *  request: json include: username, password
+ * 
+ *  Note: change return value according to display
+ */
+app.post('/userDelete', function(req, res){
+    pool.query('CALL check_login(?, ?, @val);SELECT @val AS retval',[req.body.username, req.body.password], function(err, results){
+        if(err) return res.json(err);
+        if(result[1][0].retval == 0) {
+            return res.json("Wrong username or password");
+        }
+        else {
+            pool.query('CALL delete_user(?,?)',[req.body.username, req.body.password], function(err, results){
+                if(err) return res.json(err);
+                return res.json("Delete Success");
+            })
+        }
+    })
+})
+
+
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
